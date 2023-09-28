c80107b1d954bcf5b19fed265ca1be35c8fd0c5f
sparkstein123 <hemang825vb@gmail.com>
2023-09-28 19:52:14 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb+srv://Grand_Hemang:hhhhvvvvbbbb@cluster0.u2uqsdn.mongodb.net/"
+
+const connectToMongo = async () => {
+    await mongoose.connect(mongoURI);
+    console.log("Connected to Mongo")
+};
+
+
+module.exports = connectToMongo;
